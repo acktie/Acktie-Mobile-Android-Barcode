398188597b6297ebc6d25d73a639c8ff8d575278
Tony Nuzzi <nuzzis@sbcglobal.net>
2012-10-16 21:12:30 -0500

    Fix HTC Desire issue

--- appBuild.sh
+++ appBuild.sh
@@ -1,1 +1,1 @@
-rm -rf build;ant dist;rm -rf /Library/Application\ Support/Titanium/modules/android/com.acktie.mobile.android.barcode/; unzip dist/com.acktie.mobile.android.barcode-android-1.1.zip -d /Library/Application\ Support/Titanium/;
+rm -rf build;ant dist;rm -rf /Library/Application\ Support/Titanium/modules/android/com.acktie.mobile.android.barcode/; unzip dist/com.acktie.mobile.android.barcode-android-1.2.zip -d /Library/Application\ Support/Titanium/;
